a87799723c753553808c21c89c8eb922efe38a53
Shreyashsalve <145201716+Shreyashsalve@users.noreply.github.com>
2023-09-19 00:26:28 +0530

    Add files via upload

---
+++ q5.cpp
@@ -0,0 +1,10 @@
+//WAP to find the circumference of a circle with radius 10 in C++.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r=10;
+    cout<<"Circumference of the circle is: "<<2*3.14*r;
+    return 0;
+}
